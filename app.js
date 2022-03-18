48988576518603f0dade619f8b9369122220eaad
oliviakim217 <oliviakim217@users.noreply.github.com>
2022-03-17 22:12:18 -0400

    Update credentials

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const bodyParser = require("body-parser");
 const https = require("https");
 const app = express();
 
-// apiKey = "9196a58899f730ff73d38ffce93eed72-us14"
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
